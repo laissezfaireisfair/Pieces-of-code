0b633685cccecbc8a3af771bd1e127082f55205f
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-13 13:49:32 +0700

    Shorten namespace by "using"

--- 4_test_smart_prts_and_fixed_types.cpp
+++ 4_test_smart_prts_and_fixed_types.cpp
@@ -7,8 +7,9 @@ template<class T> using sPtr = std::shared_ptr<T>;
 template<class T> using uPtr = std::unique_ptr<T>;
 
 int main() {
-  std::uint8_t size = 10;
-  sPtr<std::uint64_t[]> array(new std::uint64_t[size], std::default_delete<std::uint64_t[]>());
+  using uint = std::uint64_t;
+  uint size = 10;
+  sPtr<uint[]> array(new uint[size], std::default_delete<uint[]>());
   for (std::uint8_t i = 0; i < size; ++i)
     array[i] = std::uint64_t(i);
   for (std::uint8_t i = 0; i < size; ++i)
