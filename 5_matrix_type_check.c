b1a97f01d8143a7cace2b1144fe5c769e8731cac
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-02-26 11:29:56 +0700

    Fix matrix check code

--- 5_matrix_type_check.c
+++ 5_matrix_type_check.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void add_vals (int** matrix, int size, FILE *fin){
-    int i;
-    int j;
-    fscanf(fin, "%d", matrix[i][j]);
-} //я пытался сделать тут только одну функцию пока что и у меня нихрена не запускается, к сожалению и выдаёт кучу ошибок, кек
-int is_simmetric (int** matrix, unsigned int const size){
+void add_vals (int **matrix, unsigned int const size, FILE *fin){
+    for (unsigned int i = 0; i < size; ++i)
+      for (unsigned int j = 0; j < size; ++j)
+        fscanf(fin, "%d", &matrix[i][j]);
+}
+
+int is_simmetric (int **matrix, unsigned int const size){
     for (unsigned int i = 0; i < size; ++i)
         for (unsigned int j = 0; j < size; ++j)
             if (matrix[i][j] != matrix [j][i])
                 return 0;
     return 1;
 }
+
 int main(){
     FILE *fin = fopen("input.txt", "r");
     unsigned int size;
-    fscanf(fin, "%u", &size)
-    int p;
-    int **p = (**int)malloc(sizeof(*int)*size);
+    fscanf(fin, "%u", &size);
+    int **matrix = (int**)malloc(sizeof(int*) * size);
     for (unsigned int i = 0; i < size; ++i)
-        p[i]=(*int)malloc(sizeof(int)*size);
+        matrix[i] = (int*)malloc(sizeof(int) * size);
     add_vals(matrix, size, fin);
     fclose(fin);
-    int status_simm;
-    status_simm = is_simmetric(matrix,size);
-    FILE *fout = fout ("output.txt", "w");
-    fprintf (fout, "%d", status_simm);
+    int statusSimm = is_simmetric(matrix, size);
+    FILE *fout = fopen("output.txt", "w");
+    fprintf(fout, "%d", statusSimm);
     fclose(fout);
-    for (unsigned int i = 0; i < size; ++i );
-        free (p[i]);
-    free(p);
-    return 0
+    for (unsigned int i = 0; i < size; ++i)
+        free (matrix[i]);
+    free(matrix);
+    return 0;
 
 }
