5a11129c8d9ad55b5e2242823cfc43d2d435c620
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-09-14 13:55:47 +0700

    Add java primitive types example

---
+++ Java/2_Types/src/types.java
@@ -0,0 +1,13 @@
+public class types {
+  public static void main() {
+    final byte byteExample = 54;
+    final short shortExample = 451;
+    final int intExample = 729;
+    final long longExample = 1337;
+    final boolean booleanExample = false;
+    final double doubleExample = 3.14159265;
+    final float floatExample = 3.14f;
+    final char charExample = 'F';
+    final String stringExample = "Example";
+  }
+}
